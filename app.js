c1411d5bf3bb33ca5675271a2c6d872081748e15
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-10-26 13:02:47 -0500

    imported the arithmatical functions from a previous execise in order to create the operate function

--- app.js
+++ app.js
@@ -4,9 +4,45 @@ const a = 1; const b = 2; const c = 3; const d = 4;
 const e = 5; const f = 6; const g = 7; const h = 8;
 const eye = 9; const j = 0;
 
+//created a variable to appened the inputs to the screen
+const screen = document.querySelector('.screen');
+
+//operater functions
+let firstArgument = 0;
+let secondArgument = 0;
+
+const add = (firstArgument, secondArgument) => {
+    return firstArgument + secondArgument;
+};
+const subtract = (firstArgument, secondArgument) => {
+	return firstArgument - secondArgument;
+};
+const multiply = (firstArgument, secondArgument) => {
+    return firstArgument * secondArgument;
+}
+const divide = (firstArgument, secondArgument) => {
+    return firstArgument / secondArgument;
+}
+
+const arr = [];
+
+const sum = function(arr) {  
+  // Using reduce function to find the sum
+  const sum = arr.reduce(function (x, y) {
+      return x + y;
+  }, 0);  
+  return sum;
+};
+
+//creating the operate function
+const operate =()
+
+
+
 function one (a) {
     const oneBtn = document.querySelector('.one');
     oneBtn.addEventListener('click', () => {
+        screen.textContent = a;
         return a;     
     });    
 }
@@ -15,6 +51,7 @@ one(a);
 function two (b) {
     const twoBtn = document.querySelector('.two');
     twoBtn.addEventListener('click', () => {
+        screen.textContent = b;
         return b;     
     });    
 }
@@ -23,6 +60,7 @@ two(b);
 function three (c) {
     const threeBtn = document.querySelector('.three');
     threeBtn.addEventListener('click', () => {
+        screen.textContent = c;
         return c;     
     });    
 }
